ca79044193c9dad0a01a5df47f14d62a5d5c0b60
Dev-Arhaan <dev.sayarhaan@gmail.com>
2024-04-04 03:41:07 +0530

    testimonials completed successfully

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,8 +3,15 @@ import { Button } from "@/components/ui/button"
 import React from 'react'
 import Image from 'next/image';
 import appBanner from '../../../public/appBanner.png';
-import { CLIENTS } from '@/lib/constants';
+import { CLIENTS, USERS } from '@/lib/constants';
 import cal from '../../../public/cal.png';
+import { randomUUID } from 'crypto';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+import CustomCard from '@/components/landing-page/custom-card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { CardContent, CardDescription, CardTitle } from '@/components/ui/card';
+
 
 const HomePage = () => {
   return (
@@ -183,13 +190,54 @@ const HomePage = () => {
                 overflow-visible
                 ">
 
-                </div>
 
-          <TitleSection title="Trusted by all"
-            subheading="Join thousands of satisfied users who rely on our platform for their 
-            personal and professional productivity needs."
-            pill="Testimonials"
-          />
+            <TitleSection title="Trusted by all"
+              subheading="Join thousands of satisfied users who rely on our platform for their 
+              personal and professional productivity needs."
+              pill="Testimonials"
+              />
+              {[...Array(2)].map((arr, index) => (<div key={randomUUID()}
+              className={twMerge(clsx('mt-10 flex flex-nowrap gap-6 self-start', {
+                'flex-row-reverse': index === 1,
+                'animate-[slide_250s_linear_infinite]': true,
+                'animate-[slide_250s_linear_infinite_reverse]': index === 1,
+                'ml-[100vw]': index === 1,
+              }),
+              'hover:paused'
+              )}
+              >
+                {USERS.map((testimonial, index) => (
+                  <CustomCard 
+                  key={testimonial.name}
+                  className='w-[500px]
+                  shrink-0s
+                  rounded-xl
+                  dark:bg-gradient-to-t
+                  dark:from-border dark:to-background'
+                  cardHeader={
+                    <div 
+                    className='flex 
+                    items-center 
+                    gap-4'>
+
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">{testimonial.name}</CardTitle>
+                        <CardDescription className="dark:text--washed-purple-800">{testimonial.name.toLowerCase()}</CardDescription>
+                      </div>
+                    </div>
+                  }
+                    cardContent={<p className="dark:text-washed-purple-800">{testimonial.message}</p>}
+
+                  ></CustomCard>
+                ))}
+              </div>
+              ))}
+            </div>
         </section>
     
     
